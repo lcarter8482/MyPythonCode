a55396115930f3ca3e8ac95cdfa6381f0b27e6e7
Lindsey Carter <lc8482@mcla.edu>
2017-04-02 21:59:11 +0000

    add Exam 4 final

--- Exam4/Exam4.py
+++ Exam4/Exam4.py
@@ -10,12 +10,14 @@ def main():
     counter = search(files, userWord)
     print "\nWord Found %s times" % counter
 
+#opens all files ending in .txt in current directory
 def openFiles():
     for file in files:
         if file.endswith(".txt"):
             txtFiles.append(file)
     return
-        
+
+#reads in data from files and searches for user's word        
 def search(file, userWord):
     count = 0
     for f in txtFiles:
@@ -27,5 +29,6 @@ def search(file, userWord):
                 print "%s: %s" % (f, l.upper().strip())
                 count = count + 1
     return count
+    #returns how many times the word was found
 
 main()
