a10435b5002fc28dfacb72303c4ed91bfd2f89cf
Lindsey Carter <lc8482@mcla.edu>
2017-04-10 02:49:15 +0000

    adding assignment 7

--- MusicLibrary.py
+++ MusicLibrary.py
@@ -4,7 +4,8 @@ import Epic
 def main():
     artistName = Epic.userString("Enter An Artist Name: ")
     search(artistName)
-    
+
+#reads in a file & returns String    
 def getJson(filename):
     jsonTxt = ""
     f = open(filename)
@@ -13,17 +14,18 @@ def getJson(filename):
         jsonTxt = jsonTxt + line
     library = json.loads(jsonTxt)
     return library
-    
+
+#search function that reads through the name searched and prints specified items    
 def search(name):
     albums = []
     years = []
-    library = getJson("MusicLibrary.json")
+    library = getJson("MusicLibrary.json") #using the function to read in json file
     for artist in library:
         if artist["Artist"] == name:
             for album in artist["Albums"]:
-                albums.append(album)
+                albums.append(album) #list of all albums for each artist
             for year in artist["Year"]:
-                years.append(year)
+                years.append(year) #list of all years for each artist
     for i in range(len(albums)):
         print "%s: %s" % (albums[i], years[i])
     return
